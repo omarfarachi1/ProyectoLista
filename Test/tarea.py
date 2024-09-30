45566e7496fe8f7b84c0233d5c8f139c27eacaff
OmarFarachi <Omarfarachi@hotmail.com>
2024-09-30 19:18:54 -0300

    archivos modificados 2

--- Test/tarea.py
+++ Test/tarea.py
@@ -1,29 +1,27 @@
 class Tareas:
     def __init__(self):
-        self.lista_tareas = []
+        self.__lista_tareas = []
 
-    def agregar(self, texto):
-        if texto in self.lista_tareas:
+    def agregartarea(self, texto):
+        if texto in self.__lista_tareas:
             return False
         else:
-            self.lista_tareas.append(texto)
+            self.__lista_tareas.append(texto)
             return True
 
-    def completarTareas(self, texto):
-        if texto in self.lista_tareas:
+    def completartareas(self, texto):
+        if texto in self.__lista_tareas:
             print("Tarea completada")
             return True
         else:
             return False
 
-
-    def __listadetareas(self):
-        return self.lista_tareas
-
+    def listadetareas(self):
+        return self.__lista_tareas
 
     def eliminarTarea(self, texto):
-        if texto in self.lista_tareas:
-            self.lista_tareas.remove(texto)
+        if texto in self.__lista_tareas:
+            self.__lista_tareas.remove(texto)
             return True
         else:
             return False
--- Test/test_tarealista.py
+++ Test/test_tarealista.py
@@ -1,30 +1,32 @@
-from proyect_venv.AppLista.Test.tarea import Tareas
+from proyect_venv.applista.Test.tarea import Tareas
 
 def test_comprobar_si_agrega_una_tarea():
     app = Tareas()
+    lista = app.listadetareas()
     tarea = "realizar trabajos"
-    app.agregar(tarea)
+    app.agregartarea(tarea)
 
-    assert tarea in app.lista_tareas
+    assert tarea in lista
 
 def test_no_agregar_tarea_existente():
     app = Tareas()
     tarea1 = "tarea 1"
-    app.agregar(tarea1)
+    app.agregartarea(tarea1)
 
-    assert not app.agregar(tarea1)
+    assert not app.agregartarea(tarea1)
 
 def test_tareas_completadas():
     app = Tareas()
     tarea2 = "comprar ferreteria"
-    app.agregar(tarea2)
+    app.agregartarea(tarea2)
 
-    assert app.completarTareas(tarea2)
+    assert app.completartareas(tarea2)
 
 def test_eliminar_tareas():
     app = Tareas()
+    lista = app.listadetareas()
     tarea = "limpiar cuarto"
-    app.agregar(tarea)
+    app.agregartarea(tarea)
     app.eliminarTarea(tarea)
 
-    assert tarea not in app.lista_tareas
+    assert tarea not in lista
